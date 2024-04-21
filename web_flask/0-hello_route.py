6e23d23e2da245de6de0455049a0e641fff0ff02
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-04-21 20:45:18 +0100

    creating task 3 file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ The web application must be listening on 0.0.0.0, port 5000
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
