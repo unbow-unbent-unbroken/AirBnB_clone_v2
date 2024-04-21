319d817acba52d3abc1c32a4cbd121a2f5118318
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-04-21 20:49:37 +0100

    hot fixes

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,11 +25,14 @@ def cText(text):
     """display C followed by the value of the text variable"""
     return "C {}".format(text.replace("_", " "))
 
+
 @app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def pyText(text="is cool"):
     """display Python followed by the value of the text variable"""
     return "Python {}".format(text.replace("_", " "))
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
+    
