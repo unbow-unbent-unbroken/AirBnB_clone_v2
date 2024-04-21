413556a3fc7a34e8986fc63d133421abca7ab206
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-04-21 20:27:09 +0100

    creating task 1 file

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+The web application must listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route to display "Hello HBNB!"""
+    return ("Hello HBNB!")
+
+app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route to display "HBNB" """
+    return ("HBNB")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
