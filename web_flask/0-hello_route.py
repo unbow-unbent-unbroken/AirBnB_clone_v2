a8c0f79df09d90bc09472124fb8ec368c5e60378
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-04-21 20:08:38 +0100

    hot fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,10 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    Route to display "Hello HBNB!"
-    """
-
+    """Route to display "Hello HBNB!""""
     return ("Hello HBNB!")
 
 if __name__ == "__main__":
